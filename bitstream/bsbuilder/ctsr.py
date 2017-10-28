7c5896a23aea5f58f2c46e30f6f9171dd43d60e8
steveri <steveri@steveri.com>
2017-10-28 14:50:49 -0700

    little side project

--- bitstream/bsbuilder/ctsr.py
+++ bitstream/bsbuilder/ctsr.py
@@ -8,53 +8,73 @@ sys.path.append("../decoder")
 from lib import cgra_info
 
 def main():
+    print '''
+Plan: move this to the library I guess.
+    '''
+    test_ctsr()
+    test_ctsc()
+
+def test_ctsr():
     cgra_filename = get_default_cgra_info_filename()
     cgra_info.read_cgra_info(cgra_filename)
 
     track = 0
 
-    (begin,path,end) = connect_tiles_same_row(0,1,track)
+    (begin,path,end) = connect_tiles_same_row(0,1,track,DBG=1)
     print ""
-    (begin,path,end) = connect_tiles_same_row(0,2,track)
+
+    (begin,path,end) = connect_tiles_same_row(0,2,track,DBG=1)
     print ""
-    (begin,path,end) = connect_tiles_same_row(2,0,track)
+
+    (begin,path,end) = connect_tiles_same_row(2,0,track,DBG=1)
     print ""
-    (begin,path,end) = connect_tiles_same_row(0,5,track)
+
+    (begin,path,end) = connect_tiles_same_row(0,5,track,DBG=1)
     print ""
-    (begin,path,end) = connect_tiles_same_row(5,0,track)
+
+    (begin,path,end) = connect_tiles_same_row(5,0,track,DBG=1)
     print ""
+
     # This one crosses a memory tile in a funny way
-    (begin,path,end) = connect_tiles_same_row(8,13,track)
+    (begin,path,end) = connect_tiles_same_row(8,13,track,DBG=1)
     print ""
 
-        
+def test_ctsc():
+    cgra_filename = get_default_cgra_info_filename()
+    cgra_info.read_cgra_info(cgra_filename)
+
+    track = 0
+
+    (begin,path,end) = connect_tiles_same_col(0,8,track,DBG=1)
+    print ""
+
+    (begin,path,end) = connect_tiles_same_col(0,14,track,DBG=1)
+    print ""
+
+    (begin,path,end) = connect_tiles_same_col(14,0,track,DBG=1)
+    print ""
+
+    (begin,path,end) = connect_tiles_same_col(0,36,track,DBG=1)
+    print ""
+
+    (begin,path,end) = connect_tiles_same_col(36,0,track,DBG=1)
+    print ""
+
+    # What happens if we try to make a path through mem column?
+    (begin,path,end) = connect_tiles_same_col(3,45,track,DBG=1)
+    print ""
 
 
+def connect_tiles_same_row(src,dst,track,DBG=0):
 
-def build_wire_rc(r,c,inout,side,track):
-    tileno = cgra_info.rc2tileno(r,c)
-
-    # Need mem offset (mo) on EW sides if row is odd and tile is mem
-    mo = 0
-    if side==0 or side==2: mo=s02_mem_offset(r,c)
-    # print "# mo=", mo
-    return "T%d_%s_s%dt%d" % (tileno, inout, side+mo, track)
-
-
-
-def connect_tiles_same_row(src,dst,track):
-
-    DBG=1
     (rsrc,csrc) = cgra_info.tileno2rc(src)
     (rdst,cdst) = cgra_info.tileno2rc(dst)
     if DBG: print "# Connect tile %d (r%d,c%d) to tile %d (r%d,c%d)" %\
        (src,rsrc,csrc,dst,rdst,cdst)
     # print rdst,cdst
 
-    # tiles must be on same row;
-    # must be two different tiles
-    assert rsrc == rdst
-    assert src != dst
+    # tiles must be on same row; must be two different tiles
+    assert src != dst; assert rsrc == rdst
     
     if cdst>csrc: (inside,outside) = (2,0)
     else:         (inside,outside) = (0,2)
@@ -75,38 +95,98 @@ def connect_tiles_same_row(src,dst,track):
 
     # print cols
     for c in cols:
-
-        # Need mem offset (mo) on EW sides if row is odd and tile is mem
-        # mo=s02_mem_offset(rsrc,c)
-
         inwire  = build_wire_rc(rsrc,c,'in' ,inside, track)
         outwire = build_wire_rc(rsrc,c,'out',outside,track)
         connection = "%11s -> %12s" % (inwire, outwire)
         # print "#   ", connection
         path.append(connection)
 
-    endwire = build_wire_rc(rsrc,cdst,'in',inside,track)
+    endwire = build_wire_rc(rdst,cdst,'in',inside,track)
+    # print path
+    # print "endwire=", endwire
+    if DBG: printpath(beginwire, path, endwire)
+    return (beginwire, path, endwire)
+
+def connect_tiles_same_col(src,dst,track,DBG=0):
+
+    (rsrc,csrc) = cgra_info.tileno2rc(src)
+    (rdst,cdst) = cgra_info.tileno2rc(dst)
+    if DBG: print "# Connect tile %d (r%d,c%d) to tile %d (r%d,c%d)" %\
+       (src,rsrc,csrc,dst,rdst,cdst)
+
+    # tiles must be on same col; must be two different tiles
+    assert src != dst; assert csrc == cdst
+    
+    if rdst>rsrc: (inside,outside) = (3,1)
+    else:         (inside,outside) = (1,3)
+
+    beginwire = build_wire_rc(rsrc,csrc,'out',outside,track)
+    # print "beginwire=", beginwire
+    
+    path = []
+
+    # Could be get_beginwire(rdst,cdst,s=rsrc,d=rdst,side=inside) (vs. (csrc,cdst))
+    if abs(rsrc-rdst)==1:
+        # No need for a path; just show connection to neighbring tile
+        endwire = build_wire_rc(rdst,cdst,'in',inside,track)
+        # print "endwire=", endwire
+        if DBG: printpath(beginwire, path, endwire)
+        return (beginwire, path, endwire)
+
+    # Could be rows = get_tile_range(src=rsrc,dst=rdst) (vs. (csrc,cdst))
+    if (rdst>rsrc): rows = range(rsrc+1,rdst)
+    else:           rows = range(rsrc-1,rdst,-1)
+
+    # print cols
+    for r in rows:
+
+        # Need mem offset (mo) on EW sides if row is odd and tile is mem
+        # mo=s02_mem_offset(rsrc,c)
+
+        inwire  = build_wire_rc(r,csrc,'in' ,inside, track)
+        outwire = build_wire_rc(r,csrc,'out',outside,track)
+        connection = "%11s -> %12s" % (inwire, outwire)
+        # print "#   ", connection
+        path.append(connection)
+
+    # BOOKMARK
+    endwire = build_wire_rc(rdst,cdst,'in',inside,track)
     # print path
     # print "endwire=", endwire
     if DBG: printpath(beginwire, path, endwire)
     return (beginwire, path, endwire)
 
 
-def s02_mem_offset(r,c):
+
+
+def build_wire_rc(r,c,inout,side,track):
+    tileno = cgra_info.rc2tileno(r,c)
+
+    # Need mem offset (mo) on EW sides if row is odd and tile is mem
+    # mo = 0
+    # if side==0 or side==2:
+    mo = mem_offset(r,c,side)
+    # print "# mo=", mo
+    return "T%d_%s_s%dt%d" % (tileno, inout, side+mo, track)
+
+def mem_offset(r,c,side):
     DBG=0
 
-    # Never need offset on even rows
-    if r%2==0: return 0
+    # Never need offset for side 3
+    if side==3: return 0
 
-    # Offset of 4 if odd row and mem tile
+    # Only need offset on mem tiles
     tileno   = cgra_info.rc2tileno(r,c)
     tiletype = cgra_info.tiletype(tileno)
     if DBG: print "Tile %d has type '%s'" % (tileno,tiletype)
     if not re.search("^mem", tiletype): return 0
-    else:                               return 4
 
+    # Mem sides (0,2) need offset 4 if row is odd
+    if r%2==1 and side%2==0: return 4
 
-
+    # Mem side 1 needs offset 4 always 
+    elif side==1: return 4
+    else:         return 0
 
 # def rctype(r,c):
 #     tileno   = cgra_info.rc2tileno(r,c)
@@ -147,11 +227,5 @@ def get_default_cgra_info_filename():
     if VERBOSE: print("I think I am here:\n  %s" % mydir)
     if VERBOSE: print("Default cgra_info file is\n  %s" % cgra_filename)
     return cgra_filename
-
-
-
-
-
-
 
 main()
